ecea725cb8085b264cfa1de9c29c209bd9b39e6d
Niels Nübel <n.nuebel@nn-medienagentur.de>
2017-03-17 00:49:31 +0100

    first result

--- gulpfile.js
+++ gulpfile.js
@@ -19,37 +19,11 @@ gulp.task('build', shell.task(['jekyll build --watch']));
 // Task for serving blog with Browsersync
 gulp.task('serve', function () {
     browserSync.init({
-        server: {baseDir: '_site/'},
-        open: false
+        server: {baseDir: '_site/'}
     });
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['build', 'serve']);
-
-gulp.task('copy_files', function() {
-    gulp.src(bowerDir + '/reveal.js/lib/**/*')
-        .pipe(gulp.dest(assetsDir + '/lib'));
-    gulp.src(bowerDir + '/reveal.js/css/theme/source/**/*')
-        .pipe(gulp.dest(targetSass + '/theme/source'));
-    gulp.src(bowerDir + '/reveal.js/css/theme/template/**/*')
-        .pipe(gulp.dest(targetSass + '/theme/template'));
-    gulp.src(bowerDir + '/reveal.js/plugin/**/*')
-        .pipe(gulp.dest(assetsDir + '/plugin'));
-    gulp.src(bowerDir + '/reveal.js/js/reveal.js')
-        .pipe(gulp.dest(targetJs));
-    gulp.src(bowerDir + '/reveal.js/css/reveal.scss')
-        .pipe(rename('_reveal.scss'))
-        .pipe(gulp.dest(targetSass));
-    gulp.src(bowerDir + '/reveal.js/lib/css/zenburn.css')
-        .pipe(rename('_zenburn.scss'))
-        .pipe(gulp.dest(targetSass));
-    gulp.src(bowerDir + '/reveal.js/css/print/paper.css')
-        .pipe(rename('/print/paper.css'))
-        .pipe(gulp.dest(targetCSS));
-    gulp.src(bowerDir + '/reveal.js/css/print/pdf.css')
-        .pipe(rename('/print/pdf.css'))
-        .pipe(gulp.dest(targetCSS));
-});
 
 

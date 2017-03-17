c06715d6aa43a3bcf6f7ae4e6250021942e73fbb
Niels Nübel <n.nuebel@nn-medienagentur.de>
2017-03-17 16:04:53 +0100

    add scroll

--- js/newJS.js
+++ js/newJS.js
@@ -26,3 +26,18 @@ $(document).ready(function() {
         nextText: '<img src="images/arrow_right.png">'
     });
 });
+
+$(function() {
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top - 40
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
